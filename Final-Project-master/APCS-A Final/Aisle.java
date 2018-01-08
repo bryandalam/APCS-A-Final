f89450062a7443ff6a75520f7399d69a147e0c39
Lam <2006220@student.acalanes.k12.ca.us>
2018-01-08 10:54:52 -0800

    Worked more

---
+++ Final-Project-master/APCS-A Final/Aisle.java
@@ -0,0 +1,34 @@
+
+/**
+ * Creates an Aisle object that uses Item as an instance variable
+ *
+ * @author Bryan Lam
+ *)
+ */
+public class Aisle
+{
+    //instance variables
+    private String aisleName;
+    private Item itemName;
+    private int itemsStocked;
+    public Aisle(String nm, Item n, int itstk) {
+        //Sets the instance variables
+        this.aisleName = nm;
+        this.itemName = n;
+        this.itemsStocked = itstk;
+    }
+    public String toString() {
+        //toString method
+        return "The " + aisleName + " aisle is stocked with " + itemName + " and has " + itemsStocked + " avaliable.";
+    }
+    public boolean compareAisles(Aisle other) {
+        //compares two Aisle objects using the compareTo method and by checking for numerical equality
+        if(this.aisleName.compareTo(other.aisleName) == 0 && this.itemsStocked == other.itemsStocked) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+        
---
+++ Final-Project-master/APCS-A Final/ItemDriver.java
@@ -0,0 +1,24 @@
+
+/**
+ * Driver for the Item class and creates Item objects. It compares them too.
+ *
+ * @author Bryan Lam
+ * 
+ */
+public class ItemDriver
+{
+    public static void main(String[] args) {
+        Item orangejuice = new Item("Orange Juice", 1.00, "Tropicana");
+        Item frozenpizza = new Item("Deep Dish Pizza", 12.00, "Dominos");
+        Item chickensoup = new Item("Chicken Soup", 5.00, "Cambells");
+        changeIt(orangejuice, frozenpizza);
+        
+        
+    }
+    public static void changeIt(Item obj1, Item obj2) {
+        
+        
+        
+    }
+      
+}
